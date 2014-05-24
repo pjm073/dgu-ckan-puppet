e65380cba48c65bf683b409d5c91fa6fc37bb478
Jeffrey Payne <pjm073@gmail.com>
2014-05-24 00:39:28 -0400

    Configure ckanext.stats as namespace packages

--- src/git_clone_all.sh
+++ src/git_clone_all.sh
@@ -6,6 +6,7 @@
 git clone	git@github.com:ckan/ckan
 cd ckan
 git checkout ckan-2.2
+git checkout fa60dc947eb6db612218d8d89affec7d6df3b096
 cd -
 
 git clone	git@github.com:datagovuk/ckanext-archiver
